3b8d59625998a2f4ea3786c2eebc6b6ad1309de4
Lian13 <lian.fumerton-liu@yale.edu>
2015-04-06 02:13:56 -0400

    new

--- RummageSale/click.js
+++ RummageSale/click.js
@@ -1,19 +1,111 @@
 var state = 'off';
 
-$('#classylamp').click(function(){
+$(document).ready(function() {
+  $('#clickBalloon').click(function() {
     
+    console.log('hey');
+
+    if(state == 'off'){
+      $(this).html('<img id="balloon" src="PNGs/balloon.gif">');
+      state = 'on';
+    }
+    
+    else {
+      $(this).html('<img id="poppedBalloon" src="PNGs/pop.png">');
+      state = 'off';
+    }    
+      
+  });
+
+    $('#clickLamp').click(function() {
+    
+    console.log('hey');
+
     if(state == 'off'){
       // turn on the lights
-      $('#classylamp').css('display','none');
-      $('#classylamp2').css('display','block');
+      $(this).html('<img id="classylamp" src="PNGs/classylamp.png">');
       state = 'on';
     }
     
     else {
       // turn off the lights
-      $('#classylamp').css('display','block');
-      $('#classylamp2').css('display','none');
+      $(this).html('<img id="onlamp" src="PNGs/classylamp2.png">');
       state = 'off';
     }    
+      
+  });
+
+    $('#clickHair').click(function() {
     
+    console.log('hey');
+
+    if(state == 'off'){
+      // turn on the lights
+      $(this).html('<img id="hair" src="PNGs/hair.png">');
+      state = 'on';
+    }
+    
+    else {
+      // turn off the lights
+      $(this).html('<img id="wig" src="PNGs/wig.png">');
+      state = 'off';
+    }    
+      
+  });
+
+    $('#clickUmbrella').click(function() {
+    
+    console.log('hey');
+
+    if(state == 'off'){
+      // turn on the lights
+      $(this).html('<img id="umbrella" src="PNGs/umbrella.png">');
+      state = 'on';
+    }
+    
+    else {
+      // turn off the lights
+      $(this).html('<img id="umbrellaclosed" src="PNGs/umbrellaclosed.png">');
+      state = 'off';
+    }    
+      
+  });
+
+     $('#clickLeaves').click(function() {
+    
+    console.log('hey');
+
+    if(state == 'off'){
+      // turn on the lights
+      $(this).html('<img id="leaves" src="PNGs/leaves.png">');
+      state = 'on';
+    }
+    
+    else {
+      // turn off the lights
+      $(this).html('<img id="leavesScattered" src="PNGs/leaves2.png">');
+      state = 'off';
+    }    
+      
+  });
+
+ $('#clickEyes').click(function() {
+    
+    console.log('hey');
+
+    if(state == 'off'){
+      // turn on the lights
+      $(this).html('<img id="oldclock" src="PNGs/oldclock.png">');
+      state = 'on';
+    }
+    
+    else {
+      // turn off the lights
+      $(this).html('<img id="eyes" src="PNGs/eyes.png">');
+      state = 'off';
+    }    
+      
+  });
+
+
 });
